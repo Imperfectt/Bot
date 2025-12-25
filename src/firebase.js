05a2df91bf9bd96b17aa8d4d23b48d0520555131
Dmitriy Prokopev <83354442+Josskey@users.noreply.github.com>
2025-12-25 20:38:52 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    projectId: process.env.FIREBASE_PROJECT_ID || "",
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "",
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") || ""
   })
 });
