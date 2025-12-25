494a1ca783fe3198c42370f2779ae2f0e9d979fd
Dmitriy Prokopev <83354442+Josskey@users.noreply.github.com>
2025-12-25 20:51:40 +0500

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,7 @@
 import { Telegraf, Markup } from "telegraf";
 import dotenv from "dotenv";
 import { db } from "./firebase.js";
-import { addBet } from "./handlers/addBet.js"; // ✅ исправлено
+import { addBet } from "./handlers/addBet.js"; // ✅ правка
 import { listBets } from "./handlers/listBets.js";
 
 dotenv.config();
